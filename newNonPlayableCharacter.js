7fda0febe26d19ce5fc639617ef74d06c6c74a70
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-10-05 20:29:05 -0400

    nonplayer character movement in screen is added using async and await

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,24 +23,32 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
     function stop() {
@@ -57,3 +65,19 @@ function newNonPlayableCharacter(x, y) {
         stop: stop
     }
 }
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
+// async function test(){
+//     await sleep(5000)
+//     console.log('Hello World')
+// }
+
+
+// test()
+
+    
